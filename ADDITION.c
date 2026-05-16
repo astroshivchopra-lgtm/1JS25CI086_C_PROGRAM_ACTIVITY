4d3d48de45f29ae5de90d9ff11a51012fcc524a5
astroshivchopra-lgtm <astroshivchopra@gmail.com>
2026-05-16 12:13:26 +0530

    Create ADDITION.c

---
+++ ADDITION.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+printf("Name: SHIV CHOPRA\n");
+printf("USN: 1JS25CI086\n");
+printf("Program: Simple Addition of Two Numbers\n");
+printf("----------------------------------------\n");
+
+int a, b, sum;
+printf("Enter two numbers: ");
+scanf("%d %d", &a, &b);
+
+sum = a + b;
+printf("Sum = %d\n", sum);
+
+return 0;
+}
